574f5175328a41336d9aa06f0ebbe68f17c9b230
Trip Volpe <trip.volpe@gmail.com>
2018-05-08 20:43:42 -0700

    Add skeleton LowerHex impl / converter using core::fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,38 @@
+//! # `hexfmt`
+//!
+//! Format-controlled hexadecimal output.
+
+#![no_std]
+use core::fmt::{Formatter, LowerHex, Result};
+
+pub struct HexFmt<'a>(&'a [u8]);
+
+impl<'a> LowerHex for HexFmt<'a> {
+    fn fmt(&self, _fmtr: &mut Formatter) -> Result {
+        Ok(())
+    }
+}
+
+impl<'a, T> From<&'a T> for HexFmt<'a>
+        where T: ?Sized + AsRef<[u8]> + 'a {
+    fn from(t: &'a T) -> Self { HexFmt(t.as_ref()) }
+}
+
+pub fn hex<'a, T>(t: &'a T) -> HexFmt<'a>
+        where T: ?Sized + AsRef<[u8]> + 'a {
+    HexFmt::from(t)
+}
+
+#[cfg(test)]
+#[macro_use]
+extern crate std;
+
 #[cfg(test)]
 mod tests {
+    use ::hex;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn empty_slice_produces_empty_string() {
+        assert_eq!("", format!("{:x}", hex(&[])));
     }
 }
